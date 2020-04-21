e2fafd2f92313ed3dad41a5e6af887cd673abe37
wenqi997 <1125377852@qq.com>
2020-04-21 21:56:56 +0800

    web

--- src/main/java/com/ucd/oursql/controller/HomeController.java
+++ src/main/java/com/ucd/oursql/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.ucd.oursql.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/homepage")
+    @RequestMapping("/home")
     public String sqlUI(){
-        return "UI";
+        //return "Home";
+        ModelAndView a = new ModelAndView("Home");
+        return "redirect:Home.html";
+        //return a;
     }
 }
