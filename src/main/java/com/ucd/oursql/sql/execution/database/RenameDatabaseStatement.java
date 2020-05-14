b74999c3dcb297087f2dcf60d2fe800d71540c2a
Yuxuan Wang <yuxuanwang0829@163.com>
2020-05-14 23:31:29 +0800

    修改rename删除和存储的顺序

--- src/main/java/com/ucd/oursql/sql/execution/database/RenameDatabaseStatement.java
+++ src/main/java/com/ucd/oursql/sql/execution/database/RenameDatabaseStatement.java
@@ -60,13 +60,14 @@ public class RenameDatabaseStatement {
 
         descriptorLoader dl=new descriptorLoader();
         Database database=new Database(dl.loadFromFile(databaseName,ExecuteStatement.user.getUserName()));
-        TreeSaver tl= new TreeSaver();
-        System.out.println(databaseName);
-        tl.deleteTable(databaseName,ExecuteStatement.user.getUserName());
         Table t=database.getDatabase();
         t.getTableDescriptor().setTableName(newDatabaseName);
         descriptorSaver ds=new descriptorSaver(t.getTd(),t.getPropertyMap(),t.getTree(),ExecuteStatement.user.getUserName());
         ds.saveAll();
+        TreeSaver tl= new TreeSaver();
+//        System.out.println(databaseName);
+        tl.deleteTable(databaseName,ExecuteStatement.user.getUserName());
+
 
 //        String[] nameatt={"database"};
 //        values=new ArrayList();
