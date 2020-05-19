d491904e1f3382eaa38d5625f5b4982e04031a56
Yuxuan Wang <yuxuanwang0829@163.com>
2020-05-19 13:12:23 +0800

    jdbc 返回相关行数

--- src/main/java/com/ucd/oursql/sql/test/TestPK.java
+++ src/main/java/com/ucd/oursql/sql/test/TestPK.java
@@ -1,16 +1,21 @@
 package com.ucd.oursql.sql.test;
 
+import com.ucd.oursql.sql.login.RegristrationFunc;
+import com.ucd.oursql.sql.login.account;
 import com.ucd.oursql.sql.table.type.PrimaryKey;
 
 public class TestPK {
     public static void main(String[] args) {
-        String str="name:wyx;type:1";
-        PrimaryKey pk=new PrimaryKey();
-        try {
-            pk.setValue(str,null,null,null);
-            System.out.println(pk.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        String str="name:wyx;type:1";
+//        PrimaryKey pk=new PrimaryKey();
+//        try {
+//            pk.setValue(str,null,null,null);
+//            System.out.println(pk.toString());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        account acc=new account("user","test");
+        RegristrationFunc rf=new RegristrationFunc();
+        rf.register(acc);
     }
 }
